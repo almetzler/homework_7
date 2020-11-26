c90d430dd267a915797e769b94ff7347a1475416
Alexis Metzler <metzlera@umich.edu>
2020-11-25 23:32:01 -0500

    first nights progress

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,38 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
+    document.querySelector("#forecast").style["display"] = 'block';
 
     //Set default location if one isn't provided
     let location;
+    location = document.querySelector("#location").value;
+    if (location.length == 0){
+        location = 'ann arbor,us';
+    }
 
     // Your code here.
     console.log("Location is : " + location);
 
     //set default temperature format if one isn't provided
-    let fomat;
+    let format;
+    if (document.querySelector("#celcius").checked){
+        format = 'metric';
+    }
+    else{
+        format = 'imperial'
+    }
 
     // Your code here.
     console.log("Format is " + format);
 
     //set the query  
     let query;
+    if(isNaN(location)){
+        query = 'https://api.openweathermap.org/data/2.5/weather?q='+location+'&units='+format+'&appid=303aa92eb13b4435c439ca69778d664d';
+    }
+    else{
+        query = 'https://api.openweathermap.org/data/2.5/weather?zip='+location+'&units='+format+'&appid=303aa92eb13b4435c439ca69778d664d';
+    }
     // Your code here.  
     
     console.log("Query is :" + query);
@@ -30,10 +47,15 @@ function gettingJSON(){
 
 
     $.getJSON(query,function(json){
+        console.log(JSON.stringify(json))
         //Use returned json to update the values of the three 
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
+        loc = json['name'];
+        temp = json['main']['temp'];
 
+        document.querySelector("#loc").innerHTML = loc;
+        document.querySelector("#temp").innerHTML = temp;
     });
 }
