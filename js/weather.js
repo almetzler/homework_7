96c04563c4fe34855582769c0d7781579c091808
Alexis Metzler <metzlera@umich.edu>
2020-11-26 22:59:58 -0500

    changed default value and image

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,7 @@ function gettingJSON(){
     location = document.querySelector("#location").value;
     if (location.length == 0){
         city = 'ann arbor';
-        cntry = 'US'
+        cntry = ''
     }
     else{
         city = location.split(',')[0];
@@ -68,7 +68,7 @@ function gettingJSON(){
 
         document.querySelector("#loc").innerHTML = loc;
         document.querySelector("#temp").innerHTML = temp+' with '+altImg;
-        document.querySelector('#tempImg').src = 'http://openweathermap.org/img/wn/'+tempImg+'@2x.png';
+        document.querySelector('#tempImg').src = 'http://openweathermap.org/img/wn/'+tempImg+'.png';
         document.querySelector('#tempImg').alt = altImg;
         document.querySelector('#tempImg').title = altImg;
     });
